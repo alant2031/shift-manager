67b35dadbcf00e92732e4a03cc861357bda71a95
alan <alan@tnk.com>
2024-12-20 17:34:48 -0300

    feat: templates added

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Local apps
+    "shift",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "base_templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- shift/views.py
+++ shift/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello HOME!")
+    return render(request, "shift/home.html")
 
 
 def about(request):
-    return HttpResponse("Hello ABOUT!")
+    return render(request, "shift/about.html")
