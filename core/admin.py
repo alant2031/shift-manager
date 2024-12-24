a2bf756ce07e06f2a29116821ca7100e0c24cc37
alan <alan@tnk.com>
2024-12-24 12:50:25 -0300

    feat: start models and fields

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Account
 
-# Register your models here.
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("user", "role", "is_active", "phone_number")
--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Account(BaseModel):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, unique=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True, unique=True)
+    role = models.CharField(
+        max_length=100,
+        choices=[("admin", "Admin"), ("member", "Member")],
+        default="member",
+    )
+    is_active = models.BooleanField(default=False)
